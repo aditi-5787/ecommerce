e77818de7cf63861ef23cf44d12a5df7b691ccd0
Aditi <aditiagarwal.udr2002@gmail.com>
2024-05-02 07:21:56 +0530

    changed banner and added category cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 // Home.js
 import React from 'react';
-import Categories from '../components/Categories';
-import ProductSection from '../components/ProductSection';
+// import Categories from '../components/Categories';
+// import ProductSection from '../components/ProductSection';
 // import BannerSection from './BannerSection';
+import HeroSection from '../components/HeroSection';
 import TrendSection from '../components/TrendSection';
 import DiscountSection from '../components/DiscountSection';
 import ServiceSection from '../components/ServiceSection';
 import InstagramSection from '../components/InstagramSection';
+import CategoryCards from '../components/CategoryCards';
 
 const Home = () => {
   return (
     <div>
       <main>
-        <Categories />
-        <ProductSection />
+        <HeroSection />
+        {/* <Categories /> */}
+        <CategoryCards />
+        {/* <ProductSection /> */}
         {/* <BannerSection />*/}
         <TrendSection />
         <DiscountSection />
